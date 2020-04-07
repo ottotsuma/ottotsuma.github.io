8e0ee881788237d3fc1db19bf6a53ee81a335986
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2020-04-07 10:15:01 +0900

    Service worker updates

--- DOB-finder/service-worker.js
+++ DOB-finder/service-worker.js
@@ -28,10 +28,10 @@
         // If you have some build process for your site, perhaps that could
         // generate the list of possible URLs that a user might load.
                 '/', // do not remove this
-                '/index.html', //default
-                '/index.html?homescreen=1', //default
+                '/html2.html', //default
+                '/html2.html?homescreen=1', //default
                 '/?homescreen=1', //default
-                '/assets/css/main.css',// configure as by your site ; just an example
+                '/assets/css/app.css',// configure as by your site ; just an example
                 '/images/*',// choose images to keep offline; just an example
         // Do not replace/delete/edit the manifest.js paths below
         //These are links to the extenal social media buttons that should be cached;
