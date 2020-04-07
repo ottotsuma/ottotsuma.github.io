03a33b4541dd2e7e8ab8c6f556724f3ee6f0d201
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2020-04-07 10:29:57 +0900

    Service Web

--- DOB-finder/service-worker.js
+++ DOB-finder/service-worker.js
@@ -1,54 +1,49 @@
-// PWA Fire Bundle
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
 
-        // after a service worker is installed and the user navigates to a different page or
-        // refreshes,the service worker will begin to receive fetch events
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
 
-        self.addEventListener('fetch', function(event) {
-        event.respondWith(caches.open('cache').then(function(cache) {
-        return cache.match(event.request).then(function(response) {
-        console.log("cache request: " + event.request.url);
-        var fetchPromise = fetch(event.request).then(function(networkResponse) {
-        // if we got a response from the cache, update the cache
-        console.log("fetch completed: " + event.request.url, networkResponse);
-        if (networkResponse) {
-            console.debug("updated cached page: " + event.request.url, networkResponse);
-              cache.put(event.request, networkResponse.clone());}
-              return networkResponse;
-                  }, function (event) {
-        // rejected promise - just ignore it, we're offline!
-                  console.log("Error in fetch()", event);
-                  event.waitUntil(
-                  caches.open('cache').then(function(cache) {
-        // our cache is named *cache* in the caches.open() above
-                  return cache.addAll
-                  ([
-        //cache.addAll(), takes a list of URLs, then fetches them from the server
-        // and adds the response to the cache.
-        // add your entire site to the cache- as in the code below; for offline access
-        // If you have some build process for your site, perhaps that could
-        // generate the list of possible URLs that a user might load.
-                '/', // do not remove this
-                '/html2.html', //default
-                '/html2.html?homescreen=1', //default
-                '/?homescreen=1', //default
-                '/assets/css/app.css',// configure as by your site ; just an example
-                '/images/*',// choose images to keep offline; just an example
-        // Do not replace/delete/edit the manifest.js paths below
-        //These are links to the extenal social media buttons that should be cached;
-        // we have used twitter's as an example
-              'https://platform.twitter.com/widgets.js',
-                ]);
-                })
-                );
-                });
-        // respond from the cache, or the network
-          return response || fetchPromise;
-        });
-        }));
-        });
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
 
-        self.addEventListener('install', function(event) {
-          // The promise that skipWaiting() returns can be safely ignored.
-          self.skipWaiting();
-          console.log("Latest version installed!");
-        });
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
